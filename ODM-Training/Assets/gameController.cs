0aac50f8381f6a7ea9250dbf2bd3daba348a54b5
abuckner111 <abuckner111@gmail.com>
2021-03-23 23:32:15 -0400

    Added Pause Menu

---
+++ ODM-Training/Assets/gameController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject PausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- ODM-Training/Assets/playerControler.cs
+++ ODM-Training/Assets/playerControler.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class playerControler : MonoBehaviour
 {
+    public Transform Head;
+
+    [System.Serializable]
+    public class movementClass {
+        public float power;
+        public float strength;
+    }
+
+    public movementClass movementStats;
+    public Vector2 lookSpeed;
+
+    private Vector2 look;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,14 +28,9 @@ public class playerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        look.x -= Input.GetAxis("Mouse X");
+        look.y -= Input.GetAxis("Mouse Y");
+        transform.rotation = Quaternion.Euler(0,-look.x*lookSpeed.x,0);
+        Head.localRotation = Quaternion.Euler(look.y*lookSpeed.y,0,0);
     }
-
-    [System.Serializable]
-    public class movementClass {
-        public float power;
-        public float strength;
-    }
-
-    public movementClass movementStats;
 }
