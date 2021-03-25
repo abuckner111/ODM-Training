a2caba5c298ed4434be3ee4e436d35f1c3f57144
abuckner111 <abuckner111@gmail.com>
2021-03-24 22:32:36 -0400

    Added Pause Logic

--- ODM-Training/Assets/gameController.cs
+++ ODM-Training/Assets/gameController.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class gameController : MonoBehaviour
 {
+    public bool paused = false;
+    public bool pauseLast = true;
+
+    public void setPause(bool p)
+    {
+        paused = p;
+    }
+
+    public void OnPause()
+    {
+        PausePanel.SetActive(true);
+    }
+
+
+    public void OnResume()
+    {
+        PausePanel.SetActive(false);
+    }
+
     public GameObject PausePanel;
     // Start is called before the first frame update
     void Start()
     {
-        PausePanel.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!paused && Input.GetButton("Cancel") == true)
+        {
+            paused = true;
+        }
 
+
+        if(paused != pauseLast)
+        {
+            if(paused)
+            {
+                OnPause();
+            }
+            else
+            {
+                OnResume();
+            }
+            pauseLast = paused;
+        }
     }
 }
--- ODM-Training/Assets/playerControler.cs
+++ ODM-Training/Assets/playerControler.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class playerControler : MonoBehaviour
 {
+    public gameController pauseControl;
+    public bool pauseLast = true;
+
     public Transform Head;
 
     [System.Serializable]
@@ -28,9 +32,25 @@ public class playerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        look.x -= Input.GetAxis("Mouse X");
-        look.y -= Input.GetAxis("Mouse Y");
-        transform.rotation = Quaternion.Euler(0,-look.x*lookSpeed.x,0);
-        Head.localRotation = Quaternion.Euler(look.y*lookSpeed.y,0,0);
+        if(pauseLast != pauseControl.paused)
+        {
+            pauseLast = pauseControl.paused;
+
+            if(pauseControl.paused)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+        if(pauseControl.paused == false)
+        {
+            look.x -= Input.GetAxis("Mouse X");
+            look.y -= Input.GetAxis("Mouse Y");
+            transform.rotation = Quaternion.Euler(0,-look.x*lookSpeed.x,0);
+            Head.localRotation = Quaternion.Euler(look.y*lookSpeed.y,0,0);
+        }
     }
 }
