174e0a7842c1df15001e88586a40b1a9a6fca891
abuckner111 <abuckner111@gmail.com>
2021-04-06 21:52:44 -0400

    cable draw test

---
+++ ODM-Training/Assets/cableTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cableTest : MonoBehaviour
+{
+    public LineRenderer line;
+    public Transform    character;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, character.position);
+    }
+}
